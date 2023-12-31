6d2dac9c3d9ce32bbf3a211dc57dde1953d487ac
Chris Stayte <ctstayte@gmail.com>
2023-12-30 20:02:20 -0500

    Updated alpha deployment

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -48,103 +49,135 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: kIsWeb ? false : true,
       appBar: AppBar(
         title: const Text('Bernie The Cypher'),
       ),
       body: SafeArea(
-        child: Column(children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                maxLines: null,
-                controller: _userSubmittedText,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Enter text to cycher',
-                  contentPadding: EdgeInsets.all(10),
+        bottom: kIsWeb ? false : true,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  maxLines: null,
+                  controller: _userSubmittedText,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter text to cypher',
+                    contentPadding: EdgeInsets.all(10),
+                  ),
                 ),
               ),
             ),
-          ),
-          ListTile(
-            title: const Text('Rotation'),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    if (_rotation > 0) {
+            ListTile(
+              title: const Text('Rotation'),
+              subtitle: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('A', style: Theme.of(context).textTheme.bodyLarge),
+                  Icon(Icons.arrow_forward),
+                  Text(_rotateString('A', _rotation),
+                      style: Theme.of(context).textTheme.bodyLarge),
+                ],
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: _rotation == 0
+                        ? null
+                        : () {
+                            setState(() {
+                              _rotation--;
+                            });
+                          },
+                  ),
+                  Text(
+                    '$_rotation',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward),
+                    onPressed: () {
                       setState(() {
-                        _rotation--;
+                        _rotation++;
                       });
-                    }
-                  },
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                Text(
-                  '$_rotation',
-                  style: Theme.of(context).textTheme.bodyLarge,
+                child: SingleChildScrollView(
+                  child: _cypheredText.isEmpty
+                      ? Text(
+                          'Results will show here',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        )
+                      : SelectableText(
+                          _cypheredText,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.arrow_forward),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
                   onPressed: () {
                     setState(() {
-                      _rotation++;
+                      _userSubmittedText.clear();
+                      _cypheredText = '';
                     });
                   },
+                  child: Text('Clear'),
+                ),
+                const Gap(15),
+                FilledButton(
+                  onPressed: () {
+                    setState(() {
+                      _cypheredText = _rotateString(
+                          _userSubmittedText.text, _rotation * -1);
+                    });
+                  },
+                  child: Text('Decrypt'),
+                ),
+                const Gap(15),
+                FilledButton(
+                  onPressed: () {
+                    setState(() {
+                      _cypheredText =
+                          _rotateString(_userSubmittedText.text, _rotation);
+                    });
+                  },
+                  child: Text('Encrypt'),
                 ),
               ],
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: SingleChildScrollView(
-                child: SelectableText(
-                  _cypheredText,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FilledButton(
-                onPressed: () {
-                  setState(() {
-                    _cypheredText =
-                        _rotateString(_userSubmittedText.text, _rotation * -1);
-                  });
-                },
-                child: Text('Decrypt'),
-              ),
-              const Gap(15),
-              FilledButton(
-                onPressed: () {
-                  setState(() {
-                    _cypheredText =
-                        _rotateString(_userSubmittedText.text, _rotation);
-                  });
-                },
-                child: Text('Encrypt'),
-              ),
-            ],
-          )
-        ]),
+            kIsWeb ? const Gap(20) : const SizedBox.shrink(),
+          ],
+        ),
       ),
     );
   }
